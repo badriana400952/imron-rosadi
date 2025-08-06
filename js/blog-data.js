284d7bd6dc5121a17d0a6fce50e7fbb6a760e5d7
badriana400952 <badriana0402@gmail.com>
2025-08-06 20:53:37 +0700

    imron

---
+++ js/blog-data.js
@@ -0,0 +1,23 @@
+// blog-data.js
+const blogPosts = [
+    {
+        id: "1",
+        title: "H. Imron Rosadi, Lc. Ditetapkan sebagai Ketua DSW PKS Banten",
+        date: "July 25, 2025",
+        image: "images/a5.webp",
+        content: `
+      <p>Semoga kepemimpinan beliau membawa keberkahan dan memperkuat perjuangan dakwah serta pelayanan kepada masyarakat Banten.</p>
+      <p>Penunjukan ini menjadi harapan baru bagi kemajuan struktur dan semangat perjuangan PKS di Banten.</p>
+    `
+    },
+    {
+        id: "2",
+        title: "Why Lead Generation is Key for Business Growth",
+        date: "July 03, 2020",
+        image: "images/image_2.jpg",
+        content: `
+      <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+      <p>This is the secret of business success today!</p>
+    `
+    }
+];
